7f31b97f87c7a76cfe789b874847ff37458d0fc9
Darren <edrichdarrensantuyo172030@gmail.com>
2025-01-18 21:02:37 +0800

    extend the django authentication

--- NUMP/MP_user/admin.py
+++ NUMP/MP_user/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import Product, MarketplaceUser
+
+class MarketplaceUserInLine(admin.StackedInline):
+    model = MarketplaceUser
+    can_delete= False
+    verbose_name_plural = 'Marketplace_user'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [MarketplaceUserInLine]
 
 # Register your models here.
 admin.site.register(MarketplaceUser)
