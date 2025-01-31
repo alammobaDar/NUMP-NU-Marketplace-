193cd2787f66d2b852589c50cf773f0dce23141a
Darren <edrichdarrensantuyo172030@gmail.com>
2025-01-31 08:58:50 +0800

    included html for order and cart model

--- NUMP/MP_user/models.py
+++ NUMP/MP_user/models.py
@@ -51,7 +51,7 @@ class Order(models.Model):
 class Cart(models.Model):
     cartID = models.AutoField(primary_key=True)
     user = models.ForeignKey(MarketplaceUser, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASsCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     added_at = models.DateTimeField(auto_now_add=True)
 
--- NUMP/MP_user/views.py
+++ NUMP/MP_user/views.py
@@ -48,5 +48,5 @@ def delete(request, id):
     product.delete()
     return redirect('user:products')
 
-views
+
 
